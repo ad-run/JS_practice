7034b5534b0e03d40343cce078322b5742400d33
ad-run <auksedrun@gmail.com>
2024-01-16 11:53:47 +0100

    Statment  If userChoise is 'rock and inner If/else statment if computerChoise is  'paper' is created

--- getUserChoise.js
+++ getUserChoise.js
@@ -76,4 +76,23 @@ Let’s deal with the tie condition first. Within the determineWinner() function
     if (userChoice === computerChoice) {
         return console.log('The game was a tie.');
     }
+    
+/* 8. If the game is not a tie, you’ll need to determine a winner.
+Begin by writing an if statement that checks if the userChoice is 'rock'. 
+Inside the if statement’s block, write another if/else statement. 
+The inner if/else should check if the computerChoice is 'paper'. 
+If so, return a message that the computer won. 
+If not, return a message that the user won.*/
+    if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            return console.log('The computer won!');                    
+        } else {
+            console.log('The user won!');
+        }
+    }
+/*
+Next, write another if statement for if the userChoice is 'paper'.
+Inside this if statement, the computerChoice must be either 'scissors' or 'rock'. 
+Write logic that will return a winner. 
+*/
   }
