7e815d11c7ca03b4fed7c33e180f0cf8505ee080
ad-run <auksedrun@gmail.com>
2024-01-16 15:02:06 +0100

    calling calculateSleepDebt() function

--- sleepDebtCalculator.js
+++ sleepDebtCalculator.js
@@ -92,3 +92,44 @@ You can remove the tests when you know your functions works.*/
 
 //console.log(getActualSleepHours());
 //console.log(getIdealSleepHours());
+
+/*
+
+8. Now that you can get the actual sleep hours and the ideal sleep hours, 
+it’s time to calculate sleep debt.
+Create a function named calculateSleepDebt with no parameters.
+Inside of its block, create a variable named actualSleepHours 
+set equal to the getActualSleepHours() function call.
+Then, create another variable named idealSleepHours, 
+set equal to the getIdealSleepHours() function call.
+*/
+const calculateSleepDebt = () => {
+    const actualSleepHours = getActualSleepHours();
+    const idealSleepHours = getIdealSleepHours();
+    /*
+9.Now that you have actualSleepHours and idealSleepHours, you can write a few if/else statements to output the result to the console. The function should fulfill this logic:
+If actual sleep equals ideal sleep, log to the console that the user got the perfect amount of sleep.
+If the actual sleep is greater than the ideal sleep, log to the console that the user got more sleep than needed.
+If the actual sleep is less than the ideal sleep, log to the console that the user should get some rest.
+
+10. To make this calculator more helpful, 
+add the hours the user is over or under their ideal sleep in each log statement in 
+calculateSleepDebt().
+*/
+    if (actualSleepHours === idealHours) {
+        console.log('You got the perfect amount of sleep.');
+    } else if (actualSleepHours > idealSleepHours) {
+        console.log('You got ' + (actualSleepHours - idealSleepHours) + ' more hours sleep than needed.');
+    }
+     else 
+        console.log('You got ' + (idealSleepHours-actualSleepHours) + ' hours less sleep than you needed this week. Get some rest.');
+        ;
+
+    }
+
+/* 
+11 On the last line of the program, start the program by calling the calculateSleepDebt() function.
+*/
+
+calculateSleepDebt();
+
