4c9419e71a62af36bbb803927d1daa76d88ba3e5
Aukse1976 <auksedrun@gmail.com>
2024-01-25 16:56:14 +0100

    acessing and replacing long word with shorter one

--- grammar-checker.js
+++ grammar-checker.js
@@ -28,8 +28,7 @@ let unnecessaryWord = 'literally';
 let misspelledWord = 'beautifull';
 let badWord = 'freaking';
 
-/* 
-2. For a better visual comparison of the original and edited stories, 
+/* 2. For a better visual comparison of the original and edited stories, 
 we want to view the edited storyWords array as a string. 
 To change the storyWords array back into a readable string, 
 we can invoke the .join() method on storyWords.
@@ -39,7 +38,37 @@ to separate each array element with a space in the string.
 
 Place the .join() method invocation as an argument of a console.log() 
 statement to log the final story to the console.
-*/
+
+3. Now it’s time to start editing the story by manipulating the storyWords array. 
+We want to be able to see the changes, so be sure your console.log() of the joined story 
+is the last line of code in your editor.
+First, above the console.log() statement that uses the .join() method, 
+create a variable named 'count' that stores the number 0.
+
+Directly below count, use a .forEach() method to iterate over the storyWords array. 
+As an argument of the forEach() method, create an empty function to be used as 
+the callback function.
+
+4. For each word in the storyWords array, we want the count variable to increment 
+by one.
+Add a parameter named word to the callback function of the .forEach() method 
+to be used to store the current element when iterating over the storyWords array. 
+Each time storyWord iterates, increment count by one.
+
+Below the .forEach() method, log count to see how many words are in the story.
+
+5. A word count of 181 is a bit long for this story. 
+Let’s filter out all instances of the word “literally” to shorten 
+the story and remove the unnecessary word. We will reassign the 
+filtered story to the same storyWords variable by applying the .filter() method! 
+Throughout the project, we’ll use this approach of reassigning the storyWords 
+variable for each revision of the story.
+
+Below where you logged the 'count' variable, 
+reassign the 'storyWords' variable to equal the invocation of the .filter() 
+method on the 'storyWords' array. Give the .filter() method a callback function 
+with a parameter of word.*/
+
 let count = 0;
 
 storyWords.forEach(word => {
@@ -100,6 +129,7 @@ if it equals the badWord variable, and return the index
 of the found word.
 
 Then, log badWordIndex to the console.*/
+
 let badWordIndex = storyWords.findIndex(word => {
     if (word === badWord) {
         return word;
@@ -108,40 +138,42 @@ let badWordIndex = storyWords.findIndex(word => {
 
 console.log(badWordIndex);
 
+/* 9.Now that we have the index of the bad word, 
+we can easily replace it with something more appropriate.
+
+Access the element inside the storyWords array that has the 
+index of badWordIndex using bracket notation. 
+Set the accessed element equal to the more appropriate string, 'really'.
+
+Save the code and check that the bad word has been replaced.*/
+
 storyWords[badWordIndex] = 'really';
+/*
+10. Finally, let’s simplify the words in the story to appeal to a broader audience. 
+We can make sure every word in the story is 10 characters or less using the .every() method. 
+The .every() method uses a callback function to test if every element in an array 
+passes a specified condition. It returns true if all elements pass, 
+and false if there is an element that does not pass.
+
+To start, define a variable called 'lengthCheck' and set it to the invocation 
+of the .every() method on storyWords. 
+In the callback function, test whether every word has less than or 
+equal to 10 characters.
+
+Log lengthCheck to the console to see the result. 
+If true is logged, every word in the story has 10 characters or less. 
+Otherwise, one or more words are longer than 10 characters.*/
+
+let lengthCheck = storyWords.every(word => word.length <= 10);
+
+console.log(lengthCheck);
+
+/*11. Hmm, it looks like there’s at least one word longer than 10 characters.
+Use an iterator method of your choice to access the word (there is only one) 
+in the 'storyWords' array that is greater than 10 characters. 
+Then, manually replace the word in the original story string with a shorter word.
+Some ideas for replacement words that can be used are: stunning, dazzling, or glorious.*/
+
+storyWords[storyWords.findIndex(word => word.length > 10)] = 'dazzling';   
 
 console.log(storyWords.join(' '));
-
-/* 3.Now it’s time to start editing the story by manipulating the storyWords array. 
-We want to be able to see the changes, so be sure your console.log() of the joined story 
-is the last line of code in your editor.
-First, above the console.log() statement that uses the .join() method, 
-create a variable named 'count' that stores the number 0.
-
-Directly below count, use a .forEach() method to iterate over the storyWords array. 
-As an argument of the forEach() method, create an empty function to be used as 
-the callback function.
-
-While ES6 arrow syntax is recommended for the callback function, 
-feel free to use any syntax you’re comfortable with.
-
-4. For each word in the storyWords array, we want the count variable to increment 
-by one.
-Add a parameter named word to the callback function of the .forEach() method 
-to be used to store the current element when iterating over the storyWords array. 
-Each time storyWord iterates, increment count by one.
-
-Below the .forEach() method, log count to see how many words are in the story.
-
-5. A word count of 181 is a bit long for this story. 
-Let’s filter out all instances of the word “literally” to shorten 
-the story and remove the unnecessary word. We will reassign the 
-filtered story to the same storyWords variable by applying the .filter() method! 
-Throughout the project, we’ll use this approach of reassigning the storyWords 
-variable for each revision of the story.
-
-Below where you logged the 'count' variable, 
-reassign the 'storyWords' variable to equal the invocation of the .filter() 
-method on the 'storyWords' array. Give the .filter() method a callback function 
-with a parameter of word.*/
-
