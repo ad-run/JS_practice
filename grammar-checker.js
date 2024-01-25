97b8e128bbb6847c007fcd7858333f380bc3dc12
Aukse1976 <auksedrun@gmail.com>
2024-01-25 12:27:43 +0100

    Adding project description

---
+++ grammar-checker.js
@@ -0,0 +1,22 @@
+/*
+const numbers = [1, -1 ,2, 3];
+const items = numbers
+    .filter(n => n >= 0)
+    .map(n => ({ value: n}))
+    .filter(obj => obj.value > 1)
+    .map(obj => obj.value);
+
+console.log(items);
+*/
+/*
+Grammar Checker
+In this project, you’ll use what you know about 
+iterating over arrays to gather information and improve the quality of a paragraph.
+
+1.The 'story' variable holds the paragraph we’ll be editing. 
+In order to edit the story, we turn it into an array. 
+The .split() method separates the story string by the space character (' ') 
+and stores each word as an element of the array.
+To see the array we’ll be working with throughout the lesson, log storyWords to the console.
+After you’ve viewed the storyWords array, comment out the console.log() statement before moving to the next task.
+*/
