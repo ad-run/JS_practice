e354ecf9d35b93fb02855784595fc31e3668ffad
Aukse1976 <auksedrun@gmail.com>
2024-02-01 11:07:45 +0100

    Functions is added:  check if door is closed and if door is Chore door

--- Chore Door/script.js
+++ Chore Door/script.js
@@ -1,10 +1,35 @@
 // Accessing the elements of the door images.
 let doorImage1 = document.getElementById('door1');
-
 let doorImage2 = document.getElementById('door2');
-
 let doorImage3 = document.getElementById('door3');
 
 // Accessing start button
 
 let startButton = document.getElementsByClassName('button');
+
+// Initialising doors
+
+let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg';
+let beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg';
+let spaceDoorPath = 'https://content.codecademy.com/projects/chore-door/images/space.svg';
+let closedDoorPath = 'https://content.codecademy.com/projects/chore-door/images/closed_door.svg';
+ 
+//Checking if door is closed
+
+const isClicked = (door) => {
+    if (door.src === closedDoorPath) {
+        return true;
+    } else {
+        return false;
+    }
+}
+ // Cheking if door is Chore Door
+
+ const isBot = (door) => {
+    if (door.src === botDoorPath) {
+        return true;
+    } else {
+        return false;
+    }
+ }
+
