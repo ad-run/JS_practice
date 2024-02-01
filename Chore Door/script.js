9b358363ec2e3f2dc9b9c6df5d945a17486377a5
Aukse1976 <auksedrun@gmail.com>
2024-02-01 10:47:46 +0100

    creating variables to access doors and button

---
+++ Chore Door/script.js
@@ -0,0 +1,10 @@
+// Accessing the elements of the door images.
+let doorImage1 = document.getElementById('door1');
+
+let doorImage2 = document.getElementById('door2');
+
+let doorImage3 = document.getElementById('door3');
+
+// Accessing start button
+
+let startButton = document.getElementsByClassName('button');
